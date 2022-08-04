665315c73aa2515d6c5e00a48c14e328922e6881
Gianluca <gianluca@inovum-solutions.com>
2022-08-04 20:27:41 +0200

    add contact form logic

---
+++ script.js
@@ -0,0 +1,67 @@
+// on click submit button get the input values and send them to the server
+function formSubmit(event) {
+	event.preventDefault();
+	event.stopPropagation();
+	var name = document.querySelector('.input-name').value;
+	var email = document.querySelector('.input-email').value;
+	var phone = document.querySelector('.input-phone').value;
+	var message = document.querySelector('.input-message').value;
+	var data = { name, email, phone, message };
+	if (!name || name === '') {
+		showMessage('Please enter your name');
+		return;
+	}
+	var xhr = new XMLHttpRequest();
+	// on message sent show message in modal
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			if (JSON.parse(xhr.responseText).ok) {
+				showMessage('Thank you for getting in touch! \nWe will get back to you as soon as possible.');
+				// reset form
+				document.querySelector('.input-name').value = '';
+				document.querySelector('.input-email').value = '';
+				document.querySelector('.input-phone').value = '';
+				document.querySelector('.input-message').value = '';
+			} else if (JSON.parse(xhr.responseText).error) {
+				let errorTitle = JSON.parse(xhr.responseText).error;
+				let errors = JSON.parse(xhr.responseText).errors.map((error) => error.message).join('\n');
+				let errorMessage = `${errorTitle}`;
+				if (errorTitle != errors) {
+					errorMessage = `${errorTitle}:\n${errors}`;
+				}
+				showMessage(errorMessage);
+			}
+		}
+	};
+	// send form data to server
+	xhr.open('POST', 'https://formspree.io/f/xwkzeanq');
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(JSON.stringify(data));
+}
+
+// show message in modal
+function showMessage(message) {
+	modal.style.display = 'block';
+	document.querySelector('#modal-text').innerHTML = message;
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+	modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = 'none';
+	}
+};
+
+// trigger on click submit button
+document.querySelector('#contact-submit').addEventListener('click', formSubmit, false);
